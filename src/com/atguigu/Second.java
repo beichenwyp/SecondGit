30756ef56892139e96666848fc8e826468feb6ae
beichenwyp <624445085@qq.com>
2016-10-08 20:59:44 +0800

    Update Second.java

--- src/com/atguigu/Second.java
+++ src/com/atguigu/Second.java
@@ -3,5 +3,9 @@ package com.atguigu;
 public class Second {
 	public static void main(String[] args) {
 		System.out.println("second3423541");
+		System.out.println("second3423541");
+		System.out.println("second3423541");
+		System.out.println("second3423541");
+		System.out.println("second3423541");
 	}
 }
